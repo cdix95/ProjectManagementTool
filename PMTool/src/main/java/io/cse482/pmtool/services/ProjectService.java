45574adebba2cfed81fa34a658927d797bc2ebc8
Cameron <cdix95@gmail.com>
2019-05-03 11:45:44 -0700

    add projectId exception handling and project deletion and updating from database

--- PMTool/src/main/java/io/cse482/pmtool/services/ProjectService.java
+++ PMTool/src/main/java/io/cse482/pmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.cse482.pmtool.services;
 
 import io.cse482.pmtool.domain.Project;
+import io.cse482.pmtool.exceptions.ProjectIdException;
 import io.cse482.pmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,36 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
 
-        //Logic
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+    }
 
-        return projectRepository.save(project);
+    public Project findProjectByIdentifier(String projectId){
 
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectid){
+        Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
+
+        if (project==null){
+            throw new ProjectIdException("Cannot delete project with ID '"+projectid+"'. This project does not exist.");
+        }
+
+        projectRepository.delete(project);
     }
 }
