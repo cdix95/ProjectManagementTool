03bf71ba03cdcb28e31e024b821b451a081828ee
Cameron Dix <cdix95@gmail.com>
2019-06-04 16:00:09 -0700

    implement user registration

--- PMTool/src/main/java/io/cse482/pmtool/web/UserController.java
+++ PMTool/src/main/java/io/cse482/pmtool/web/UserController.java
@@ -1,4 +1,43 @@
 package io.cse482.pmtool.web;
 
+import io.cse482.pmtool.domain.User;
+import io.cse482.pmtool.services.MapValidationErrorService;
+import io.cse482.pmtool.services.UserService;
+import io.cse482.pmtool.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
+        //validate passwords match
+        userValidator.validate(user, result);
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null)return errorMap;
+
+        User newUser = userService.saveUser(user);
+
+        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+    }
 }
